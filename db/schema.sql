66e6fbc58f8854334e9ea143b1ba555938c233ea
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-04-19 15:18:41 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(30),
+    devoured TINYINT(1),
+	PRIMARY KEY (id)
+);
